8b3f4d2ea5af206aadcf6200fa3521e7d1ff3d77
Juan Rodriguez <satanasdelcolo@gmail.com>
2024-10-26 03:37:23 -0400

    carga de HTML y configuracion de menu

---
+++ web/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', dashboard, name='dashboard'),
+    path('crear_producto', crear_producto, name='crear_producto'),
+    path('crear_usuario', crear_usuario, name='crear_usuario'),
+    path('ingresar_venta', ingresar_venta, name='ingresar_venta'),
+    path('perfil_usuario', perfil_usuario, name='perfil_usuario'),
+    path('todas_las_ventas', todas_las_ventas, name='todas_las_ventas'),
+    path('todos_los_productos', todos_los_productos, name='todos_los_productos'),
+    path('todos_los_usuarios', todos_los_usuarios, name='todos_los_usuarios'),
+]
+
+
+
+
--- web/views.py
+++ web/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+def dashboard ( request ):
+    return render( request, 'web/dashboard-admin.html' )
+
+def crear_producto ( request ):
+    return render( request, 'web/crear_producto.html' )
+
+def crear_usuario ( request ):
+    return render( request, 'web/crear_usuario.html' )
+
+def ingresar_venta ( request ):
+    return render( request, 'web/ingresar_venta.html' )
+
+def perfil_usuario ( request ):
+    return render( request, 'web/perfil_usuario.html' )
+
+def todas_las_ventas ( request ):
+    return render( request, 'web/todas_las_ventas.html' )
+
+def todos_los_productos ( request ):
+    return render( request, 'web/todos_los_productos.html' )
+
+def todos_los_usuarios ( request ):
+    return render( request, 'web/todos_los_usuarios.html' )
+
+
+
+
+
+
+
+
